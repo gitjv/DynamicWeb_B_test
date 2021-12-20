d6aa5c973808b5fbb0747c239b6fb26510dd2ad7
gitjv <gitjv@vitlin.com>
2021-12-20 20:08:16 +1100

    new stories

--- docs/dwr_db_I_story_0.js
+++ docs/dwr_db_I_story_0.js
@@ -2,7 +2,7 @@
 
 I_story_0 = [
 "",
-"",
+"<div>\n<div class=\"grampsstylednote\">\n<p>\nCottontail was formally adopted by Flopsy after poor little Cottontails's mother was eaten by the fox.\n</p>\n</div>\n</div>",
 "<div>\n<div class=\"grampsstylednote\">\n<p>\nFlopsy Bunny was born in the northern meadow.  She lost her family to foxes, and was taken in by her Aunt Jemima.\n</p>\n<p>\nAfter working in the fields, she went to school, and then married Peter Rabbit.  She has never been very far away from the meadow.\n</p>\n</div>\n</div>",
 "",
 "",
@@ -19,7 +19,7 @@ I_story_0 = [
 "",
 "",
 "",
-"",
+"<div>\n<div class=\"grampsstylednote\">\n<p>\nRalph Rabbit is the only known rabbit from his litter.  The others all disappeared.\n</p>\n</div>\n</div>",
 "",
 "",
 ""
